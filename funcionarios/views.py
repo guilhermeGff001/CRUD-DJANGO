9d7056cd0a8336dcba8ef0bf4f3c85ff10454bb5
guilhermeGff001 <gc.guilherme.contato@gmail.com>
2024-04-21 22:57:23 -0300

    Adicionando a listagem de Funcionarios

--- funcionarios/views.py
+++ funcionarios/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Funcionario
 
 # Create your views here.
+def funcionarios_list(request):
+    funcionarios = Funcionario.objects.all
+    return render(request,"funcionarios/funcionarios_list.html",{"funcionarios": funcionarios})
+    
